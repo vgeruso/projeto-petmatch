69125db26f83c44da772d95fbf4d7b7d983a9f61
prikasouzadev <prikasouza.dev@gmail.com>
2026-03-17 16:40:19 -0300

    core: ajustando a branch para o novo projeto

--- server/src/database/connection.ts
+++
@@ -1,5 +0,0 @@
-import { drizzle } from "drizzle-orm/bun-sql";
-import { SQL } from "bun";
-
-const connection = new SQL("postgresql://postgres:123456@localhost:5432/db_petmatch")
-export const db = drizzle(connection, {logger: true});
